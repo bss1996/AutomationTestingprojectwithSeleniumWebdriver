9400ba406c1ac802501168637a78e3c4245c3ace
Bhawani singh <shekhawat.bs13@gmail.com>
2023-11-29 12:41:28 +0530

    some changes in code

--- src/main/java/selenium/UpdatedDropdown.java
+++ src/main/java/selenium/UpdatedDropdown.java
@@ -14,13 +14,30 @@ public class UpdatedDropdown {
 		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
 		driver.findElement(By.id("divpaxinfo")).click();
 			Thread.sleep(2000L);
-		int i=1;
-	while (i<5)
-	{
-		driver.findElement(By.id("hrefIncAdt")).click();//4 time
-		i++;
-	}
-		driver.findElement(By.id("btnclosepaxoption")).click();
+		// int i=1;
+
+	
+	// while (i<5)
+	// {
+	// 	driver.findElement(By.id("hrefIncAdt")).click();//4 time
+	// 	i++;
+	// }
+	// 	driver.findElement(By.id("btnclosepaxoption")).click();
+	// }
+
+
+
+// we can run this code with for and while loop
+
+		System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
+for(int i=1;i<5;i++) 
+{
+	driver.findElement(By.id("hrefIncAdt")).click();
+}
+	driver.findElement(By.id("btnclosepaxoption")).click();
+
+			System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
+
 	}
 
 }
