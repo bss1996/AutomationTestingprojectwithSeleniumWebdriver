e5fd7fd7dfa4e93a77f19fbb12f112af6661a19f
bss1996 <130886867+bss1996@users.noreply.github.com>
2023-12-03 19:05:47 +0530

    Add files via upload

---
+++ CucumberFramework/src/test/java/TestRunner/Run.java
@@ -0,0 +1,20 @@
+package TestRunner;
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = ".//Features/Login.feature",
+		glue="StepDefinition",
+		dryRun = true,
+		monochrome = true,
+		plugin = {"pretty"}
+		
+		)
+
+
+public class Run {
+// this class will be empty
+}
